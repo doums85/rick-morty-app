e73c6c0d095b1b257f43a611bcebb506d002f4b3
Doums <momoseck8@gmail.com>
2022-09-26 14:16:11 +0200

    firt commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
+// https://rickandmortyapi.com/api/character
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://rickandmortyapi.com/api/character/?name=')
+      .then((response) => {
+        if (!response.ok) {
+          //  throw new Error(`🚨 : ${response.status}`);
+          if (response.status.toString().startsWith(4)) {
+            console.log('ERROR : Document no found');
+          } else {
+            console.log('Server Down try later');
+          }
+
+          return;
+        }
+        console.log(response);
+        return response.json();
+      })
+      .then((responseData) => setData(responseData.results));
+  }, []);
+
+  console.log(data[0]);
+  // BEM
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map(({ name, image, status, gender }) => (
+        <div className="card">
+          <img
+            src={image}
+            alt={`character Rick and Morty called ${name}`}
+            className="card__image"
+          />
+
+          <h3 className="card__name">{name}</h3>
+          <p>{gender}</p>
+          <small>{status}</small>
+        </div>
+      ))}
     </div>
   );
 }
